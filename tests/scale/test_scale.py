b8dac749743e81b93f0b701e99236934971a8fcc
Miguel Ferreira <amigueloferreira@gmail.com>
2022-08-18 12:28:16 +0000

    added horizontal/vertical flip tests

--- tests/scale/test_scale.py
+++ tests/scale/test_scale.py
@@ -18,6 +18,18 @@ def square_image_grayscale():
         ]
         ,dtype=np.float32)
 
+@pytest.fixture
+def square_image_grayscale_asymmetric():
+    return np.array(
+        [
+        [1,2,3,4,5],
+        [5,4,3,2,1],
+        [1,2,3,4,5],
+        [1,2,3,4,5],
+        [5,4,3,2,1],
+        ]
+        ,dtype=np.float32)
+
 def test_scale_down_center_pivot(square_image_grayscale,default_stream):
 
     op = Scale(scale=[0.5,0.5],pivot=[0.5,0.5],fillMode=FillMode.CONSTANT,stream=default_stream)
@@ -67,6 +79,42 @@ def test_scale_down_br_pivot(square_image_grayscale,default_stream):
         [0,0,1,1,1],
         [0,0,1,1,1],
         [0,0,1,1,1]
+        ]
+        ,dtype=np.float32)
+
+    assert np.array_equal(result,expected_result)
+
+def test_scale_hflip(square_image_grayscale_asymmetric,default_stream):
+
+    op = Scale(scale=[-1.0,1.0],pivot=[0.5,0.5],fillMode=FillMode.CONSTANT,stream=default_stream)
+
+    result = op.run(square_image_grayscale_asymmetric).cpu().numpy()
+
+    expected_result = np.array(
+        [
+        [5,4,3,2,1],
+        [1,2,3,4,5],
+        [5,4,3,2,1],
+        [5,4,3,2,1],
+        [1,2,3,4,5]
+        ]
+        ,dtype=np.float32)
+
+    assert np.array_equal(result,expected_result)
+
+def test_scale_vflip(square_image_grayscale_asymmetric,default_stream):
+
+    op = Scale(scale=[1.0,-1.0],pivot=[0.5,0.5],fillMode=FillMode.CONSTANT,stream=default_stream)
+
+    result = op.run(square_image_grayscale_asymmetric).cpu().numpy()
+
+    expected_result = np.array(
+        [
+        [5,4,3,2,1],
+        [1,2,3,4,5],
+        [1,2,3,4,5],
+        [5,4,3,2,1],
+        [1,2,3,4,5],
         ]
         ,dtype=np.float32)
 
